cdc9e1eef49af53cf4c2e6aaaa0e89ac786fec7e
elaitala <elaitala@gmail.com>
2020-03-09 12:08:46 -0700

    setting up server.js file. Adds index files for routes

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
-const express = require('exzpress');
+const express = require('express');
 const bodyParser = require('body-parser');
 
 // ENV File
+require('dotenv').config();
 const PORT = process.env.PORT;
 const routes = require('./routes');
 
@@ -13,6 +14,13 @@ app.use(bodyParser.json());
 app.get('/', (req, res) => {
   res.send('<h1>Welcome to BWR</h1>')
 })
+
+app.use('/api/v1/:user', (req, res) => {
+  res.send('<h1>This is the USER PROFILE</h1>')
+})
+
+
+
 
 app.listen(PORT, () =>
   console.log(`Server connected at http://localhost:${PORT}`)
